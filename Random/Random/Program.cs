b4419ed9f9726c6ef9026520697f2568f5516e3c
ShahaneVardazaryan798 <58977401+ShahaneVardazaryan798@users.noreply.github.com>
2020-04-17 01:47:36 +0400

    258

---
+++ Random/Random/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using static System.Console;
+
+namespace RandomLesson
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            var str = new char[5];
+            start: for (int i = 0; i < str.Length; i++)
+            {
+
+
+
+                str[0] = chars[rd.Next(0, 28)];
+                str[i] = chars[rd.Next(28, 57)];
+                str[i] = chars[rd.Next(chars.Length)];
+                WriteLine(str[i]);
+                
+            }   
+                   
+            Thread.Sleep(1000);
+                goto start;
+            
+
+
+
+        }
+    }
+}
